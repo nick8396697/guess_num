eff0e24c453d924255486e102be1e2e1938b50d2
Hong Xie <nick8396697@gmail.com>
2021-10-05 23:23:22 +0800

    add count

--- guess.py
+++ guess.py
@@ -7,20 +7,19 @@ max = int(max)
 
 num = random.randint(min, max)
 
-times = 0
+count = 0
 while True:
-	times = times + 1
+	count += 1 #count = count + 1
 	guess = input('請猜數字: ')
 	guess = int(guess)
 	if guess == num:
 		print('恭喜你猜對了!')
-		print('你總共猜了', times, '次')
+		print('這是你猜的第', count, '次')
 		break
 	elif guess > num and guess <= max:
 		print('猜錯囉!再猜小一點')
-		print('你已經猜了', times, '次')
 	elif guess < num and guess >= min:
 		print('猜錯囉!再猜大一點')
-		print('你已經猜了', times, '次')
 	else:
 		print('只能猜', min, '~', max)
+	print('這是你猜的第', count, '次')
